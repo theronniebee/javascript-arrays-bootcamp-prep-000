7b574231aceac0b815d397eb83aa0cfb36ad5499
Laronda Bell <larondak.bell@gmail.com>
2019-07-19 21:37:16 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -6,3 +6,6 @@ function destructivelyAddElementToBeginningOfArray(chocolateBars, candyString){
   chocolateBars.unshift(candyString);
   return chocolateBars;
 }
+function addElementToEndOfArray(chocolateBars, candyString){
+  return [...chocolateBars, candyString];
+}
