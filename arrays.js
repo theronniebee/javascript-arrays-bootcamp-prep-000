32ec2956035e5293762ebb57b9bb2d8af46b6d15
Laronda Bell <larondak.bell@gmail.com>
2019-07-19 21:36:28 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -2,3 +2,7 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 function addElementToBeginningOfArray(chocolateBars,candyString){
   return [ candyString, ...chocolateBars ];
 }
+function destructivelyAddElementToBeginningOfArray(chocolateBars, candyString){
+  chocolateBars.unshift(candyString);
+  return chocolateBars;
+}
